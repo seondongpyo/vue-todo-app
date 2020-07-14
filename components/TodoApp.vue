082b3c604113b0f55d0bac55051a87d6527dd504
Omnibus <psd1390@gmail.com>
2020-07-14 23:02:54 +0900

    Local DB 설정 및 초기화

--- components/TodoApp.vue
+++ components/TodoApp.vue
@@ -6,6 +6,10 @@
 </template>
 
 <script>
+// 로컬 스토리지
+import lowdb from 'lowdb';
+import LocalStorage from 'lowdb/adapters/LocalStorage';
+
 import TodoCreator from './TodoCreator';
 import TodoItem from './TodoItem'
 
@@ -13,6 +17,27 @@ export default {
     components: {
         'todo-creator': TodoCreator,
         'todo-item': TodoItem
+    },
+    data () {
+        return {
+            db: null
+        }
+    },
+    created () {
+        this.initDB();
+    },
+    methods: {
+        initDB () {
+            const adapter = new LocalStorage('todo-app');   // 'todo-app'이라는 이름으로 어댑터를 생성
+            this.db = lowdb(adapter);
+
+            // Local DB 초기화
+            this.db
+                .defaults({
+                    todos: []   // Collection
+                })
+                .write();
+        }
     }
 }
 </script>
