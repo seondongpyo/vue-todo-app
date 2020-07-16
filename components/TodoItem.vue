a5a791699799aee761555ae17c90b2a3abbc77c7
Omnibus <psd1390@gmail.com>
2020-07-16 23:46:17 +0900

    할 일 컴포넌트 스타일 적용

--- components/TodoItem.vue
+++ components/TodoItem.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="todo-item">
+    <div 
+        :class="{ done: done }"
+        class="todo-item"
+    >
 
         <div
             v-if="isEditMode" 
@@ -125,3 +128,20 @@ export default {
     }
 }
 </script>
+
+<style scoped lang="scss">
+    .todo-item {
+        margin-bottom : 10px;
+        .item__inner {
+            display: flex;
+        }
+        .item__date {
+            font-size: 12px;
+        }
+        &.done {
+            .item__title {
+                text-decoration: line-through;
+            }
+        }
+    }
+</style>
