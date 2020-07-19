1ef022797f884f50edc32e52db11298c1d516d47
Omnibus <psd1390@gmail.com>
2020-07-19 13:10:41 +0900

    vue router 기본 설정

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import App from './App';
+import router from './router/index';
 
 // 최상위 컴포넌트
 new Vue({
     el: '#app',
+    router: router, // 라우터 추가
     render: h => h(App)
     
     // 위와 동일함
---
+++ router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import Home from '../views/Home'
+import About from '../views/About'
+import TodoApp from '../views/TodoApp'
+
+// vue-router 사용
+Vue.use(VueRouter);
+
+const routes = [
+    // config
+    {
+        path: '/',
+        component: Home
+    },
+    {
+        path: '/about',
+        component: About
+    },
+    {
+        path: '/todos',
+        component: TodoApp
+    }
+
+]
+
+export default new VueRouter({
+    routes: routes
+});
+
