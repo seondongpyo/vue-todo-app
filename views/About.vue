b4a6c3262e9c6b1205ad946b95861b80204a7d23
Omnibus <psd1390@gmail.com>
2020-07-19 13:31:31 +0900

    about 내용 추가

---
+++ views/About.vue
@@ -0,0 +1,11 @@
+<template>
+    <section class="about">
+        <div>
+            <h2>Seondong Pyo</h2>
+            <br>
+            <h2>psd1390@gmail.com</h2>
+            <br>
+            <h2>010-0000-0000</h2>
+        </div>
+    </section>
+</template>
